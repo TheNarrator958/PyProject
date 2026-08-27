06d3d2fb5a9a3022cd2ec2f375befe676160ee18
Dominic Lyu <lyudominic@gmail.com>
2026-08-27 16:56:07 -0400

    Added Encounter 3

--- PyProject/PyProject.py
+++ PyProject/PyProject.py
@@ -418,7 +418,7 @@ while moveOnEn2 == False:
     print( "|-----------------Actions-----------------|")
     print(f"| (h) increase max health - 14 gold       |")
     print(f"|   (g) increase guard points - 12 gold   |")
-    print(f"| (a) increase attack power - 10 gold      |")
+    print(f"| (a) increase attack power - 10 gold     |")
     print(f"|  (p) buy potions (x1) - 6 gold          |")
     print(f"|  (m) move onto next encounter           |")
     print( "|-----------------Actions-----------------|")
@@ -445,6 +445,180 @@ while moveOnEn2 == False:
         increaseAttackPower = (atkPwr * 0.25) + random.randint(2, 6)
         atkPwr += increaseAttackPower
         gold -= 10
+        print(f"Your attack power has been increased by {increaseAttackPower:.2f}")
+
+    # buy potions
+    if shopEn1 == "p":
+        doublePotions = random.randint(1, 2)
+        gold -= 6
+
+        if doublePotions == 1:
+            potions += 1
+            print("You have bought 1 potion")
+        if doublePotions == 2:
+            potions += 2
+            print("Congrats! The shopkeeper decided to give you a 2nd potion for the price of 1!")
+
+    # move onto next encounter
+    if shopEn1 == "m":
+        moveOnEn2 = True
+
+    input()
+#endregion
+
+#ENCOUNTER 3
+#region
+zombieHealth = 75
+zombieAtkPower = 8
+
+ranAwayFromZombie = False
+
+while zombieHealth > 0:
+    if health > maxHealth:
+        health = maxHealth
+
+    if health <= 0:
+        os.system('cls')
+        print("You've died!")
+        input()
+        print("You have failed the journey and thus the experiment. You may close the game now.")
+        sys.exit()
+
+    os.system('cls')
+    print("- Encounter 2 of 5 -")
+    print(f"Zombie | HP: {zombieHealth:.2f} | Power: {zombieAtkPower}")
+    print( "|-------Actions-------|")
+    print(f"| (a)ttack  |  (h)eal |")
+    print(f"|   (g)uard | (r)un   |")
+    print( "|-------Actions-------|")
+    print(f"{name} | HP: {health:.2f} / {maxHealth:.2f} | Potions Left: {potions}\n")
+
+    en1 = input()
+
+    # attack
+    if en1 == "a":
+        zombieHealth -= atkPwr
+
+        if health < (maxHealth/2):
+            dmg = (zombieAtkPower / 0.25)
+        if health >= (maxHealth / 1.25):
+            dmg = (zombieAtkPower / 0.3)
+
+        a_rand = random.randint(1, 2)
+
+        if a_rand == 1:
+            dmg -= random.randint(1, 3)
+        if a_rand == 2:
+            dmg += random.randint(1, 3)
+
+        health -= dmg
+        print(f"You deal {atkPwr} damage! However, the Zombie attacks back dealing {dmg:.2f} damage!")
+
+    # heal
+    if en1 == "h":
+        if potions <= 0:
+            potions = 0
+            print("You reach for a flask, however, you discover that you have run out!")
+        if potions > 0:
+            potions -= 1
+
+            if health < (maxHealth / 2):
+                regainHealth = (health * 0.25 + 5)
+            if health >= (maxHealth / 2):
+                regainHealth = (health * 0.25 + 2)
+
+            h_rand = random.randint(1, 2)
+
+            if h_rand == 1:
+                regainHealth -= random.randint(1, 2)
+            if h_rand == 2:
+                regainHealth += random.randint(1, 2)
+
+            health += regainHealth
+            print(f"You reach for a flask and pop open the cork. Taking a swig you regain {regainHealth}!")
+
+    # guard
+    if en1 == "g":
+        g_dmgCalc = zombieAtkPower + (zombieAtkPower * 2.5)
+        g_dmgCalc -= shield
+        
+        if g_dmgCalc < 0:
+            g_dmgCalc = random.randint(1, 3)
+
+        health -= g_dmgCalc
+
+        print(f"The zombie reaches out, tackling you as hard as it can!\nThe zombie deals {g_dmgCalc} damage!")
+
+    # run
+    if en1 == "r":
+        canRunAway = random.randint(1, 2)
+
+        if canRunAway == 1:
+            print("You ran away from the zombie successfully! You move onto the next room.")
+            input()
+            ranAwayFromZombie = True
+            break
+        if canRunAway == 2:
+            print("You attempt to sprint past the zombie into the next room, however, the zombie slaps you back into the far wall and keeps you boxed in!")
+            
+            r_dmgCalc = zombieAtkPower + (zombieAtkPower * 2.5)
+            r_dmgCalc -= shield
+        
+            if r_dmgCalc < 0:
+                r_dmgCalc = random.randint(1, 3)
+
+            health -= r_dmgCalc
+            print(f"You take {r_dmgCalc} damage!")
+        
+    input()
+
+os.system('cls')
+
+if ranAwayFromZombie == False:
+    print(f"Congrats! You beat the zombie!")
+    award = random.randint(30, 45)
+    gold += award
+    print(f"You are awarded {award} gold!")
+    input()
+
+
+#SHOP BEFORE ENCOUNTER 4
+moveOnEn2 = False
+
+while moveOnEn2 == False:
+    os.system('cls')
+    
+    print("SHOP")
+    print( "|-----------------Actions-----------------|")
+    print(f"| (h) increase max health - 15 gold       |")
+    print(f"|   (g) increase guard points - 13 gold   |")
+    print(f"| (a) increase attack power - 11 gold     |")
+    print(f"|  (p) buy potions (x1) - 6 gold          |")
+    print(f"|  (m) move onto next encounter           |")
+    print( "|-----------------Actions-----------------|")
+    print(f"{name} | HP: {health:.2f} / {maxHealth:.2f} | Shield: {shield:.2f} | Attack Power: {atkPwr:.2f} \n Potions Left: {potions} | Gold: {gold}\n")
+
+    shopEn1 = input()
+
+    # increase max health
+    if shopEn1 == "h":
+        increaseHealth = (maxHealth * 0.25) + random.randint(5, 10)
+        maxHealth += increaseHealth
+        gold -= 15
+        print(f"Your max health has been increased by {increaseHealth:.2f}")
+
+    # increase guard points/shield
+    if shopEn1 == "g":
+        increaseGuard = (shield * 0.25) + random.randint(4, 8)
+        shield += increaseGuard
+        gold -= 13
+        print(f"Your guard points has been increased by {increaseGuard:.2f}")
+
+    # increase attack power
+    if shopEn1 == "a":
+        increaseAttackPower = (atkPwr * 0.25) + random.randint(2, 6)
+        atkPwr += increaseAttackPower
+        gold -= 11
         print(f"Your attack power has been increased by {increaseAttackPower:.2f}")
 
     # buy potions
